2bba853d73100ebf3fc05c9af2cc07366018cc5b
Mark Seemann <mark@ploeh.dk>
2017-07-06 14:29:50 +0200

    Change from NUnit to xUnit.net
    This commit also checks in binaries for NuGet packages, which is on
    purpose:
    http://blog.ploeh.dk/2014/01/29/nuget-package-restore-considered-harmful

--- Tests.cs
+++ Tests.cs
@@ -1,66 +1,65 @@
-﻿using NUnit;
-using NUnit.Framework;
+﻿
+using Xunit;
 
 namespace TDDMoney {
-  [TestFixture]
-  class With_Money {
-    [Test]
+  public class With_Money {
+    [Fact]
     public void Currency_name() {
-      Assert.AreEqual("USD", Money.Dollar(1).Currency);
-      Assert.AreEqual("CHF", Money.Franc(1).Currency);
+      Assert.Equal("USD", Money.Dollar(1).Currency);
+      Assert.Equal("CHF", Money.Franc(1).Currency);
     }
 
-    [Test]
+    [Fact]
     public void Multiplication() {
       Money dollarFive = Money.Dollar(5);
       
-      Assert.AreEqual(Money.Dollar(10), dollarFive.Times(2));
-      Assert.AreEqual(Money.Dollar(15), dollarFive.Times(3));
+      Assert.Equal(Money.Dollar(10), dollarFive.Times(2));
+      Assert.Equal(Money.Dollar(15), dollarFive.Times(3));
     }
 
-    [Test]
+    [Fact]
     public void Equality() {
-      Assert.IsTrue(Money.Dollar(5).Equals(Money.Dollar(5)));
-      Assert.IsFalse(Money.Dollar(5).Equals(Money.Dollar(6)));
-      Assert.IsFalse(Money.Franc(5).Equals(Money.Dollar(5)));
+      Assert.True(Money.Dollar(5).Equals(Money.Dollar(5)));
+      Assert.False(Money.Dollar(5).Equals(Money.Dollar(6)));
+      Assert.False(Money.Franc(5).Equals(Money.Dollar(5)));
     }
 
-    [Test]
+    [Fact]
     public void Simple_addition() {
       Bank bank = new Bank();
       Money five = Money.Dollar(5);
       IExpression sum = five.Plus(five);
       Money reduced = bank.Reduce(sum, "USD");
 
-      Assert.AreEqual(Money.Dollar(10), reduced);
+      Assert.Equal(Money.Dollar(10), reduced);
     }
 
-    [Test]
+    [Fact]
     public void Plus_should_return_Sum() {
       Money five = Money.Dollar(5);
       IExpression result = five.Plus(five);
       Sum sum = (Sum)result;
-      Assert.AreEqual(five, sum.Augend);
-      Assert.AreEqual(five, sum.Addend);
+      Assert.Equal(five, sum.Augend);
+      Assert.Equal(five, sum.Addend);
     }
 
-    [Test]
+    [Fact]
     public void Reduce() {
       Bank bank = new Bank();
       Money result = bank.Reduce(Money.Dollar(1), "USD");
-      Assert.AreEqual(Money.Dollar(1), result);
+      Assert.Equal(Money.Dollar(1), result);
     }
 
-    [Test]
+    [Fact]
     public void Reduce_different_currencies() {
       Bank bank = new Bank();
       bank.AddRate("CHF", "USD", 2);
 
       Money result = bank.Reduce(Money.Franc(2), "USD");
-      Assert.AreEqual(Money.Dollar(1), result);
+      Assert.Equal(Money.Dollar(1), result);
     }
 
-    [Test]
+    [Fact]
     public void Mixed_addition() {
       IExpression fiveBucks = Money.Dollar(5);
       IExpression tenFrancs = Money.Franc(10);
@@ -68,38 +67,37 @@ namespace TDDMoney {
       bank.AddRate("CHF", "USD", 2);
       Money result = bank.Reduce(fiveBucks.Plus(tenFrancs), "USD");
 
-      Assert.AreEqual(Money.Dollar(10), result);
+      Assert.Equal(Money.Dollar(10), result);
     }
   }
 
-  [TestFixture]
-  class With_Sum {
-    [SetUp]
-    public void Setup() {
+  public class With_Sum {
+    public With_Sum()
+    {
       m_bank.AddRate("CHF", "USD", 2);
     }
 
-    [Test]
+    [Fact]
     public void Reduce() {
       IExpression sum = new Sum(Money.Dollar(3), Money.Dollar(4));
       Money result = m_bank.Reduce(sum, "USD");
-      Assert.AreEqual(Money.Dollar(7), result);
+      Assert.Equal(Money.Dollar(7), result);
     }
 
-    [Test]
+    [Fact]
     public void Plus_Money() {
       IExpression sum = new Sum(m_fiveBucks, m_tenFrancs).Plus(m_fiveBucks);
       Money result = m_bank.Reduce(sum, "USD");
 
-      Assert.AreEqual(Money.Dollar(15), result);
+      Assert.Equal(Money.Dollar(15), result);
     }
 
-    [Test]
+    [Fact]
     public void Times() {
       IExpression sum = new Sum(m_fiveBucks, m_tenFrancs).Times(2);
       Money result = m_bank.Reduce(sum, "USD");
 
-      Assert.AreEqual(Money.Dollar(20), result);
+      Assert.Equal(Money.Dollar(20), result);
     }
 
     private Bank m_bank = new Bank();
@@ -107,11 +105,10 @@ namespace TDDMoney {
     private IExpression m_tenFrancs = Money.Franc(10);
   }
 
-  [TestFixture]
-  class With_Bank {
-    [Test]
+  public class With_Bank {
+    [Fact]
     public void Identity_rate_should_be_1() {
-      Assert.AreEqual(1, new Bank().Rate("USD", "USD"));
+      Assert.Equal(1, new Bank().Rate("USD", "USD"));
     }
   }
 }
